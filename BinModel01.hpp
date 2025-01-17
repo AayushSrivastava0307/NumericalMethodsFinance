e9d6e9aae419f5b7ada9e46aaf1f859a059bfdb0
Aayush Srivastava <aayush.srivastava0307@gmail.com>
2025-01-17 12:31:35 +0530

    initial commit

---
+++ BinModel01.hpp
@@ -0,0 +1,10 @@
+#ifndef BinModel01_hpp
+#define BinModel01_hpp
+// computing risk-neutral probability
+double RiskNeutProb(double U, double D, double R);
+// computing the stock price at node n,i
+double S(double S0, double U, double D, int n, int i);
+// inputting, displaying and checking model data
+int GetInputData(double &S0,
+                 double &U, double &D, double &R);
+#endif
